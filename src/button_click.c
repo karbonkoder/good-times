65f6f07889631330510629248e2acf47cf07de55
araina <araina@groupon.com>
2015-03-30 09:14:08 +0530

    Added #defines which gover rendering on screen and buffer sizes

--- src/button_click.c
+++ src/button_click.c
@@ -19,12 +19,17 @@ static TextLayer *text_layer_bad_times;
 
 static GRect view_grect[3];
 
-#define BUFFER_SIZE 200
+#define CHAR_PER_LINE 25
+#define MAX_LINES 5
+#define WORDS_PER_LINE 4
+#define HEIGHT_NOW 12
+
+#define BUFFER_SIZE (CHAR_PER_LINE*MAX_LINES + WORDS_PER_LINE)
 static char buffer_good[BUFFER_SIZE];
 static char buffer_bad[BUFFER_SIZE];
 
 // Model
-#define SIZE 17
+#define SIZE (WORDS_PER_LINE*MAX_LINES + 1)
 static time_t model_good_times[SIZE];
 static int model_good_size = 0;
 static time_t model_bad_times[SIZE];
@@ -53,7 +58,7 @@ static void view_render_model(TextLayer *text_layer_view, time_t* model, int siz
     tick_time = localtime(&model[i]);
     strftime(tm_buffer, 10, "%H%M%S", tick_time);
     strcat(buffer, tm_buffer);
-    if ((i + 1) % 4 == 0) {
+    if ((i + 1) % WORDS_PER_LINE == 0) {
       strcat(buffer, "\n");
     }
   }
@@ -62,14 +67,12 @@ static void view_render_model(TextLayer *text_layer_view, time_t* model, int siz
 }
 
 static void view_set_grect(GRect bounds) {
-  // 20 for now. remaining between good and bad
   // height. goes into 3. Good | Now | Bad
-  int height_now = 20;
-  int height_good_or_bad = (bounds.size.h - height_now)/2;
+  int height_good_or_bad = (bounds.size.h - HEIGHT_NOW)/2;
 
   view_grect[0] = (GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, height_good_or_bad } };
-  view_grect[1] = (GRect) { .origin = { 0, height_good_or_bad }, .size = { bounds.size.w, height_now } };
-  view_grect[2] = (GRect) { .origin = { 0, height_good_or_bad + height_now }, .size = { bounds.size.w, height_good_or_bad } };
+  view_grect[1] = (GRect) { .origin = { 0, height_good_or_bad }, .size = { bounds.size.w, HEIGHT_NOW } };
+  view_grect[2] = (GRect) { .origin = { 0, height_good_or_bad + HEIGHT_NOW }, .size = { bounds.size.w, height_good_or_bad } };
 }
 
 static void update_time() {
